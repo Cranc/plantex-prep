e466cc72e12ac0e9ad921664df702d23d160ace0
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2016-06-30 22:20:59 +0200

    Make one ci script slightly more idiomatic
    ... or not. Who knows?

--- ci/check-rustfmt.sh
+++ ci/check-rustfmt.sh
@@ -6,7 +6,7 @@ echo "=== Checking Rust style with rustfmt... =============="
 ERROR=0
 for f in $(find . -regex '.+src/.+\.rs'); do
     # jesus I'm sorry, but I couldn't find a better way to use rustfmt :/
-    if test $(rustfmt --write-mode=checkstyle --skip-children $f | grep 'error' | wc -l) != 0; then
+    if [ $(rustfmt --write-mode=checkstyle --skip-children $f | grep 'error' | wc -l) != 0 ]; then
         echo "! incorrectly formatted: $f"
         ERROR=1
     fi
