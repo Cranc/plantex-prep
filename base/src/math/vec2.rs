a9a368f03522fddd3046f959037f31942cf73b30
Jonas Schievink <jonas@schievink.net>
2016-07-16 16:48:49 +0200

    Vec2 tuning
    * Derive Clone and Copy
    * Implement Add/Sub for Vec2 operations
    * Add a few tests

--- base/src/math/vec2.rs
+++ base/src/math/vec2.rs
@@ -2,14 +2,11 @@ use num_traits::Zero;
 use std::ops;
 use std::fmt;
 
-#[derive(PartialEq, Eq, Hash)]
+#[derive(PartialEq, Eq, Hash, Copy, Clone)]
 pub struct Vec2<T> {
     pub x: T,
     pub y: T,
 }
-
-// impl<T: Copy> ::std::marker::Copy for Vec2<T> {}
-// impl<T: Clone> ::std::clone::Clone for Vec2<T> {}
 
 impl<T> Vec2<T> {
     pub fn new(x: T, y: T) -> Self {
@@ -60,3 +57,36 @@ macro_rules! impl_scalar_ops {
 impl_scalar_ops!(Mul, mul);
 impl_scalar_ops!(Div, div);
 impl_scalar_ops!(Rem, rem);
+
+macro_rules! impl_vec_ops {
+    ($op_trait:ident, $op_fn:ident) => {
+        impl<T, U> ops::$op_trait<Vec2<U>> for Vec2<T>
+            where T: ops::$op_trait<U>,
+                  U: Clone
+        {
+            type Output = Vec2<<T as ops::$op_trait<U>>::Output>;
+            fn $op_fn(self, rhs: Vec2<U>) -> Self::Output {
+                Vec2::new(
+                    ops::$op_trait::$op_fn(self.x, rhs.x),
+                    ops::$op_trait::$op_fn(self.y, rhs.y),
+                )
+            }
+        }
+    }
+}
+
+impl_vec_ops!(Add, add);
+impl_vec_ops!(Sub, sub);
+
+#[test]
+fn scalar_ops() {
+    assert_eq!(Vec2::new(3, 2) * 2, Vec2::new(6, 4));
+    assert_eq!(Vec2::new(3, 2) / 2, Vec2::new(1, 1));
+    assert_eq!(Vec2::new(3, 2) % 2, Vec2::new(1, 0));
+}
+
+#[test]
+fn vec_ops() {
+    assert_eq!(Vec2::new(3, 3) + Vec2::new(2, -4), Vec2::new(5, -1));
+    assert_eq!(Vec2::new(0, 2) - Vec2::new(2, -4), Vec2::new(-2, 6));
+}
